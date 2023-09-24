10fef799c5b02939566425fc3c1a1d06387df954
scrim666 <zoz341995@gmail.com>
2023-09-24 15:24:14 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let name =prompt("What is your Name");
+
+//! error handling the empty name:
+
+if (name !== null) {
+    
+alert("Welcome "+name+"!")
+
+}
+
+let age = prompt("How old are you?");
+
+
+if (age <15) {
+
+    alert("you cannot Login the webpage Sorry!")
+
+}
+    else {  alert("Welcome to our Website 🤍")
+    
+}
+
+console.log();  
